0ee9fd74b72036c9ba21eb49aa56bc83eb49b13a
avinashnakka <avinashnakka93@gmail.com>
2019-06-11 00:47:01 +0100

    Collection View and App Icon Added

---
+++ MatchGame/Card.swift
@@ -0,0 +1,18 @@
+//
+//  Card.swift
+//  MatchGame
+//
+//  Created by Avinash Nakka on 6/10/19.
+//  Copyright © 2019 Avinash Nakka. All rights reserved.
+//
+
+import Foundation
+
+class Card{
+    
+    var imagename = ""
+    var isFlipped = false
+    var isMatched = false
+    
+    
+}
---
+++ MatchGame/CardModel.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  MatchGame
+//
+//  Created by Avinash Nakka on 6/10/19.
+//  Copyright © 2019 Avinash Nakka. All rights reserved.
+//
+
+import Foundation
+
+class CardModel {
+    
+    func getCards() {
+        
+    
+    }
+}
